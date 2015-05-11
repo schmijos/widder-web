f16df3e2a5edfe5ab02994f755e8860eb88fee15
Josua Schmid <josua.schmid@renuo.ch>
2015-05-11 18:24:09 +0200

    remove dead code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,9 +17,4 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.find(params[:id])
   end
-
-  # Only allow a trusted parameter "white list" through.
-  def article_params
-    params.require(:article).permit(:title, :content)
-  end
 end
--- app/controllers/visions_controller.rb
+++ app/controllers/visions_controller.rb
@@ -3,16 +3,4 @@ class VisionsController < ApplicationController
   def index
     @visions = Vision.all
   end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_vision
-    @vision = Vision.find(params[:id])
-  end
-
-  # Only allow a trusted parameter "white list" through.
-  def vision_params
-    params.require(:vision).permit(:title, :content)
-  end
 end
