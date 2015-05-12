7acec3b150470f4620e4a1c5d3e6730653f56e08
Josua Schmid <josua.schmid@renuo.ch>
2015-05-12 18:36:55 +0200

    added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Article.create title: 'First Article', content: 'This is the first article content'
+Article.create title: 'Second Article', content: 'This is the second article content'
+
 Vision.create caption: 'First Vision', content: 'This is the first vision content'
+Vision.create caption: 'Second Vision', content: 'This is the second vision content'
+
 Image.create caption: 'Lulz Image', image_id: 'https://placehold.it/120x120'
+Image.create caption: 'Lol Image', image_id: 'https://placehold.it/120x120'
