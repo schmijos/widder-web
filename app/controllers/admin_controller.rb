d4fab9f07d47bdd59125157a252ea2fa3bb76176
Josua Schmid <josua.schmid@renuo.ch>
2015-05-26 19:36:43 +0200

    added simple admin auth

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,11 @@
 class AdminController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password:  ENV['ADMIN_PASS']
+
   layout 'admin'
 
   # GET /admin
   def index
   end
+
+
 end
